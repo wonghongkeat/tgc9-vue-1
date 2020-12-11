21698b5fc75b64d3a831c4a5a399b7ae25b14cdb
wonghongkeat <hongkeat1991@gmail.com>
2020-12-11 05:59:08 +0000

    edited

---
+++ 08-list-redux/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <FaultList/>
+  </div>
+</template>
+
+<script>
+import FaultList from './components/FaultList'
+
+export default {
+  name: 'App',
+  components: {
+      FaultList
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 09-advance-todo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <FaultList/>
+  </div>
+</template>
+
+<script>
+import FaultList from './components/ToDoList'
+
+export default {
+  name: 'App',
+  components: {
+      FaultList
+  }
+}
+</script>
+
+<style>
+
+</style>
